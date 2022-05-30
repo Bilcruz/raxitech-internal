5122d76d7aaeacef9160d4a63f1d7ac73c4b835c
Bilcruz <muhammed.omeiza@microbridge.com.ng>
2022-05-30 14:24:16 +0100

    initial commit

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Paragraph = ()=>{
+    return (
+        <div className="flex gap-10 items-center justify-between bg-blue-200  flex-row md:w-3/4 mx-auto mt-20 mb-20">
+
+
+        <div className="w-full p-5 text-justify" >
+      <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eos
+      nobis ipsa totam ad enim vitae, necessitatibus voluptas vero iusto
+      quam laborum animi inventore beatae repellendus odio delectus.
+      Odio necessitatibus, libero adipisci nam pariatur consectetur
+      deserunt consequuntur ea ratione amet
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eos
+      nobis ipsa totam ad enim vitae, necessitatibus voluptas vero iusto
+      quam laborum animi inventore beatae repellendus odio delectus.
+      Odio necessitatibus, libero adipisci nam pariatur consectetur
+      deserunt consequuntur ea ratione amet
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eos
+      nobis ipsa totam ad enim vitae, necessitatibus voluptas vero iusto
+      quam laborum animi inventore beatae repellendus odio delectus.
+      Odio necessitatibus, libero adipisci nam pariatur consectetur
+      deserunt consequuntur ea ratione amet
+        </p>
+      </div>
+      <div className="bg-blue-200  w-full h-[20rem] bg-cover bg-img4">
+       {/* <img className="w-3/4 h-full bg-contain  px-5" src= {picture}  ></img> */}
+      </div>
+      
+     
+      </div>
+
+    )
+    
+}
+export default Paragraph;
